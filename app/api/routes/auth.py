1e360f3134083a6ac21bce601087777b164b8513
jong-k <fully.charged07@gmail.com>
2024-10-01 17:43:17 +0900

    feat: 라우터에서 이메일 확인 추가

--- app/api/routes/auth.py
+++ app/api/routes/auth.py
@@ -7,4 +7,7 @@ router = APIRouter()
 
 @router.post("/email")
 async def simple_send(email: EmailRequest):
-    return {"message": email.email}
+    if email.email.split("@")[1] == "inu.ac.kr":
+        return {"message": "인천대학교 이메일을 확인했습니다"}
+
+    return {"message": "이메일을 확인했습니다"}
