0ec09d8fffcd2bc9771655cd774e37df004672e0
jong-k <fully.charged07@gmail.com>
2024-10-01 22:52:12 +0900

    feat: 이메일 example 추가

--- app/models/auth.py
+++ app/models/auth.py
@@ -3,3 +3,11 @@ from pydantic import BaseModel, EmailStr
 
 class EmailRequest(BaseModel):
     email: EmailStr
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "email": "sample@inu.ac.kr",
+            }
+        }
+    }
